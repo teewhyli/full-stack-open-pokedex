d0016b7c302479004c1faf603da44ac2df10e32e
T.Y Li <t.y.li@xero.com>
2023-09-17 15:24:19 +1200

    add Ivysaur profile clicks through correctly e2e test

--- cypress/e2e/Pokedex.cy.js
+++ cypress/e2e/Pokedex.cy.js
@@ -9,4 +9,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('Ivysaur profile clicks through correctly', function() {
+    cy.visit('http://localhost:5000')
+    cy.get('a[href*="ivysaur"]').click()
+    cy.contains('.pokemon-ability-name', 'chlorophyll').should('exist')
+  })
 })
