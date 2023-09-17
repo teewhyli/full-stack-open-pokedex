edc36f234b3de337a5c22514a67b5b4dfc1daa04
T.Y Li <t.y.li@xero.com>
2023-09-17 14:47:48 +1200

    fixing imports and port assignmetn

--- cypress/e2e/Pokedex.cy.js
+++ cypress/e2e/Pokedex.cy.js
@@ -1,6 +1,7 @@
 describe('Pokedex', function() {
+  const PORT = process.env.PORT || 5000
   it('front page can be opened', function() {
-    cy.visit('http://localhost:8080')
+    cy.visit(`http://localhost:${PORT}`)
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
